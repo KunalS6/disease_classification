d0d934aa803e22b4e0d22314b7c7c2a2a9d119b3
Kunal Samanta <samantakunal6@gmail.com>
2023-06-04 19:41:44 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+with open("readme.md",'r',encoding='utf-8') as f:
+    long_descriprtion=f.read()
+
+
+__version__='0.0.0'
+
+REPO_NAME='disease_classification'
+AUTHOR_USER_NAME='KunalS6'
+SRC_REPO='cnnClassifier'
+AUTHOR_EMAIL='samantakunal6@gmail.com'
+
+
+
+
